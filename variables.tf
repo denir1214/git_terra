dbb8f9fc2d0ceb55aefac94a0923f09a54216887
denir1214 <garinef373@sskaid.com>
2026-04-14 00:39:33 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,10 +12,4 @@ variable "public_subnet2_name" {}
 variable "public_subnet3_name" {}
 # variable "private_subnet_name" {}
 variable "Main_Routing_Table" {}
-variable "azs" {
-  description = "Run the EC2 Instances in these Availability Zones"
-  type        = list(string)
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
-}
-variable "environment" { default = "dev" }
-variable "env" {}
+
